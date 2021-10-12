234c5524264bbe5a9dc28a4e00b0888ef4ae459d
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-10-12 12:28:48 +0300

    added the test to the main method

--- basiclibrary/lib/src/main/java/basiclibrary/Library.java
+++ basiclibrary/lib/src/main/java/basiclibrary/Library.java
@@ -8,7 +8,21 @@ import java.util.Arrays;
 public class Library {
 
     public static void main(String[] args) {
-        System.out.println("Debug your code and test here");
+        System.out.println("Calling the roll method with 5 trails " + Arrays.toString(roll(5)));
+        System.out.println("Does the [1, 2, 3, 4, 5, 5, 7, 8] contain duplicate item: " + containsDuplicates(new Integer[]{1, 2, 3, 4, 5, 5, 7, 8}));
+        System.out.println("Does the [1, 2, 3, 4, 5, 6, 7, 8] contain duplicate item: " + containsDuplicates(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}));
+        System.out.println("What is the Average of this array [1, 3, 7, 6, 8, 1, 5, 0, 15, 22, 35, 78, 99, 105, 258]: " + calculatingAverages(new int[]{1, 3, 7, 6, 8, 1, 5, 0, 15, 22, 35, 78, 99, 105, 258}));
+        System.out.println("We have this two dimensional Array \n[[66, 64, 58, 65, 71, 57, 60],\n" + "[57, 65, 65, 70, 72, 65, 51],\n" + "[55, 54, 60, 53, 59, 57, 61],\n" + "[65, 56, 55, 52, 55, 62, 57]] \nand the array that contains the lowest average among them is:" + Arrays.toString(arraysOfArraysAverages(new int[][]{
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        })) + "\nand it's average: " + calculatingAverages(arraysOfArraysAverages(new int[][]{
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        })));
 
     }
 
